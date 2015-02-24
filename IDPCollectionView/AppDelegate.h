b04c69c31d42f73c8990fec9d8d37c27d557ed56
Artem Chabanniy <artem.chabanniy@gmail.com>
2015-02-24 10:23:38 +0200

    Added project.

---
+++ IDPCollectionView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IDPCollectionView
+//
+//  Created by Artem Chabanniy on 2/24/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
+
---
+++ IDPCollectionView/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  IDPCollectionView
+//
+//  Created by Artem Chabanniy on 2/24/15.
+//  Copyright (c) 2015 IDAP Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
